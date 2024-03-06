d414c335f1e5474ec2e62635bd48bd0d64487638
Matthias Sunday Oduh <sundayoduhmatthias@gmail.com>
2024-03-06 03:41:51 +0100

    fixed index and pycodestyle error

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,20 +2,21 @@
 
 """Module implements a function that returns pascal Triangle"""
 
+
 def pascal_triangle(n):
-    """returns pascal list of lists containing pascal triangle for a given value of n"""
+    """returns pascal list of lists containing pascal triangle
+    for a given value of n"""
     pascal_list = []
     if n <= 0:
         return pascal_list
     pascal_list.append([1])
-    
     for i in range(1, n):
-        temp_list = []
-        temp_list.append(1)
+        temp_list = [1]
         for j in range(1, i):
-            sum_of_adjacent_elements = pascal_list[i - 1][j - 1] + pascal_list[i - 1][j]
+            current = pascal_list[i - 1][j - 1]
+            adjacent = pascal_list[i - 1][j]
+            sum_of_adjacent_elements = current + adjacent
             temp_list.append(sum_of_adjacent_elements)
         temp_list.append(1)
-    pascal_list.append(temp_list)
-
+        pascal_list.append(temp_list)
     return pascal_list
